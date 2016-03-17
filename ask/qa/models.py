6467643a9f83557ba81fc722cd5cdb7296e3c1c5
const <const77@inbox.ru>
2016-03-17 06:31:33 +0300

    Level 2.6 update

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -9,7 +9,7 @@ class Question(models.Model):
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User,default=1)
     #likes = models.ManyToManyField(User, related_name='questions', blank=True)
     likes = models.TextField()
 
@@ -24,4 +24,4 @@ class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, default=1)

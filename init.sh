e2bb3249aa34a185268169ab691b6c1289fb49c2
pythonbalm <78608269+pythonbalm@users.noreply.github.com>
2021-03-14 12:13:18 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,8 +4,3 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
-
-sudo /etc/init.d/mysql start
-mysql -uroot -e "create database qa"
-#gunicorn -b 0.0.0.0:8080 hello:app
-#./script.sh

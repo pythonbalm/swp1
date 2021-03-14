4ad459a4090a938a32773295a4a608ff222d33da
pythonbalm <78608269+pythonbalm@users.noreply.github.com>
2021-03-14 14:24:53 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-cd /home/box/web/ask
-gunicorn -c /home/box/web/etc/gunicorn_ask.conf ask.wsgi:application &
+#cd /home/box/web/ask
+#gunicorn -c /home/box/web/etc/gunicorn_ask.conf ask.wsgi:application &

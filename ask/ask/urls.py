e23a91d186cacb8dc289ce9828f6ee257b9f108a
pythonbalm <78608269+pythonbalm@users.noreply.github.com>
2021-03-14 12:21:04 +0400

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from qa.views import test
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'qa.views.test', name='home'),
-    url(r'^login/', 'qa.views.test'),
-    url(r'^signup', 'qa.views.test'),
-    url(r'^question/(?P<id>\d+)', 'qa.views.test', name='question'),
-    url(r'^ask/', 'qa.views.test', name='ask'),
-    url(r'^popular/', 'qa.views.test', name='popular'),
-    url(r'^new', 'qa.views.test'),
-    url(r'^answer/$', 'qa.views.test', name='answer'),
+    url(r'^$', 'test', name='home'),
+    url(r'^login/', 'test'),
+    url(r'^signup', 'test'),
+    url(r'^question/(?P<id>\d+)', 'test', name='question'),
+    url(r'^ask/', 'test', name='ask'),
+    url(r'^popular/', 'test', name='popular'),
+    url(r'^new', 'test'),
+    url(r'^answer/$', 'test', name='answer'),
 )

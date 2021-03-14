eb98de5680b8990c97faed9388a5d68e328604fb
pythonbalm <78608269+pythonbalm@users.noreply.github.com>
2021-03-14 13:22:10 +0400

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,17 +1,23 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from django.contrib import admin
 from qa.views import test
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'test', name='home'),
-    url(r'^login/', 'test'),
-    url(r'^signup', 'test'),
-    url(r'^question/(?P<id>\d+)', 'test', name='question'),
-    url(r'^ask/', 'test', name='ask'),
-    url(r'^popular/', 'test', name='popular'),
-    url(r'^new', 'test'),
-    url(r'^answer/$', 'test', name='answer'),
-)
+urlpatterns = [
+
+    url(r'^$', test),
+
+    url(r'^init25/', test),
+    url(r'^login/', test),
+    url(r'^signup/', test),
+    url(r'^ask/', test),
+    # url(r'^answer/', answer),
+    url(r'^popular/', test),
+    url(r'^new/', test),
+
+    url(r'^admin/', admin.site.urls),
+    url(r'^question/', include('qa.urls')),
+
+    url(r'^', not_found),
+]

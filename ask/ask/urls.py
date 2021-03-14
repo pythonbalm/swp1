bf44d1879f31d134a37fc1ad1585a21c64e39edf
pythonbalm <78608269+pythonbalm@users.noreply.github.com>
2021-03-14 15:05:06 +0400

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     url(r'^signup/', test),
     url(r'^ask/', test),
     url(r'^popular/', test),
-    url(r'^new', 'test'),
+    url(r'^new', test),
     url(r'^question/(?P<pk>\d+)/$', test),
 ]
